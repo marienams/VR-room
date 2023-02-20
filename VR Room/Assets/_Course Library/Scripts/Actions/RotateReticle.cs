6526e6dba8a0859b8da2710f12c13aaee95f8da9
Mahima SIngh <mahima.ms0007@gmail.com>
2023-02-20 13:48:25 +0100

    added anchors, reticle, rotating reticle
    vr locomotion 1.2, extension activities hard applied

---
+++ VR Room/Assets/_Course Library/Scripts/Actions/RotateReticle.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateReticle : MonoBehaviour
+{
+    public float Speed;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 3 * Speed, 0, Space.World);
+    }
+}
